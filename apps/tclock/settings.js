c37bc9ebaefe01594c8ac5792064e1bf333b1796
2302585092 <230.2585092@web.de>
2020-05-08 21:16:20 +0200

    fix tclock settings

---
+++ apps/tclock/settings.js
@@ -0,0 +1,34 @@
+// make sure to enclose the function in parentheses
+(function(back) {
+  let settings = require('Storage').readJSON('tclock.json',1)||{};
+  function save(key, value) {
+    settings[key] = value;
+    require('Storage').write('tclock.json',settings);
+  } 
+  const appMenu = {
+    '': {'title': 'App Settings'},
+    '< Back': back,
+    'Date': {
+      value: settings.date||false,
+      format : date => date?"Yes":"No",
+      onchange: (date) => {save('date', date)}
+    },
+    'Alarm1 active': {
+      value: settings.alarm1||false,
+      format : alarm1 => alarm1?"Yes":"No",
+      onchange: (alarm1) => {save('alarm1', alarm1)}
+    },
+    "alarm time min" : {
+      value : settings.amin,
+      min: 0, max: 59, step: 1,
+      onchange : saveChange('amin', amin)
+    },
+    "alarm time hr" : {
+      value : settings.ahr,
+      min: 0, max: 23, step: 1,
+      onchange : saveChange('ahr', ahr)
+    }
+  };
+  E.showMenu(appMenu)
+})
+
