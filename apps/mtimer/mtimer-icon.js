a83f6cd2934daad2c5049878c99d1445bad4b768
2302585092 <64748910+2302585092@users.noreply.github.com>
2020-05-08 09:29:56 +0200

    Create mtimer-icon.js

---
+++ apps/mtimer/mtimer-icon.js
@@ -0,0 +1,1 @@
+E.toArrayBuffer(atob("ICCBAf/////////////nM///4yP//8IH///AD///yAP//8gD//wMB//gDB//gAcH/wADw/4zMAP8czAP+AAA//AAAP/jMzD/5zMx/+AAAf/AAAH/zzMz/88zM//AACf/wAAH/+czD//jMx//8TI///gwf//8Af///wf/////////////"))
