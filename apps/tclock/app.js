04d2bb570e85da4a36d951559a50cdbae073c890
2302585092 <230.2585092@web.de>
2020-05-11 21:32:48 +0200

    add tclock widget

--- apps/tclock/app.js
+++ apps/tclock/app.js
@@ -8,8 +8,6 @@ let settings = Storage.readJSON(filename,1) || {
 };
 
 const locale = require('locale');
-const faceWidth = 100; // watch face radius (240/2 - 24px for widget area)
-const widgetHeight=24+1;
 const startX = 30;
 const startYSec = 100;
 const startYMin = 70;
@@ -29,66 +27,66 @@ let timer = null;
 let currentDate = new Date();
 
 const words = [
-  'null', 
-  'eins', 
-  'zwei', 
-  'drei', 
-  'vier', 
-  'fÃ¼nf', 
-  'sechs',
-  'sieben',
-  'acht',
-  'neun',
-  'zehn',
-  'elf',
-  'zwÃ¶lf',
-  'dreizehn',
-  'vierzehn',
-  'fÃ¼nfzehn',
-  'sechzehn',
-  'siebzehn',
-  'achtzehn',
-  'neunzehn',
-  'zwanzig',
-  'einundzwanzig',
-  'zweiundzwanzig',
-  'dreiundzwanzig',
-  'vierundzwanzig',
-  'fÃ¼nfundzwanzig',
-  'sechsundzwanzig',
+  'null            ',
+  'eins            ',
+  'zwei            ',
+  'drei            ',
+  'vier            ',
+  'fünf            ',
+  'sechs           ',
+  'sieben          ',
+  'acht            ',
+  'neun            ',
+  'zehn            ',
+  'elf             ',
+  'zwölf           ',
+  'dreizehn        ',
+  'vierzehn        ',
+  'fünfzehn        ',
+  'sechzehn        ',
+  'siebzehn        ',
+  'achtzehn        ',
+  'neunzehn        ',
+  'zwanzig         ',
+  'einundzwanzig   ',
+  'zweiundzwanzig  ',
+  'dreiundzwanzig  ',
+  'vierundzwanzig  ',
+  'fünfundzwanzig  ',
+  'sechsundzwanzig ',
   'siebenundzwanzig',
-  'achtundzwanzig',
-  'neunundzwanzig',
-  'dreiÃŸig',
-  'einunddreiÃŸig',
-  'zweiunddreiÃŸig',
-  'dreiunddreiÃŸig',
-  'vierunddreiÃŸig',
-  'fÃ¼nfunddreiÃŸig',
-  'sechsunddreiÃŸig',
-  'siebenunddreiÃŸig',
-  'achtunddreiÃŸig',
-  'neununddreiÃŸig',
-  'vierzig',
-  'einundvierzig',
-  'zweiundvierzig',
-  'dreiundvierzig',
-  'vierundvierzig',
-  'fÃ¼nfundvierzig',
-  'sechsundvierzig',
+  'achtundzwanzig  ',
+  'neunundzwanzig  ',
+  'dreißig         ',
+  'einunddreißig   ',
+  'zweiunddreißig  ',
+  'dreiunddreißig  ',
+  'vierunddreißig  ',
+  'fünfunddreißig  ',
+  'sechsunddreißig ',
+  'siebenunddreißig',
+  'achtunddreißig  ',
+  'neununddreißig  ',
+  'vierzig         ',
+  'einundvierzig   ',
+  'zweiundvierzig  ',
+  'dreiundvierzig  ',
+  'vierundvierzig  ',
+  'fünfundvierzig  ',
+  'sechsundvierzig ',
   'siebenundvierzig',
-  'achtundvierzig',
-  'neunundvierzig',
-  'fÃ¼nfzig',
-  'einundfÃ¼nfzig',
-  'zweiundfÃ¼nfzig',
-  'dreiundfÃ¼nfzig',
-  'vierundfÃ¼nfzig',
-  'fÃ¼nfundfÃ¼nfzig',
-  'sechsundfÃ¼nfzig',
-  'siebenundfÃ¼nfzig',
-  'achtundfÃ¼nfzig',
-  'neunundfÃ¼nfzig'
+  'achtundvierzig  ',
+  'neunundvierzig  ',
+  'fünfzig         ',
+  'einundfünfzig   ',
+  'zweiundfünfzig  ',
+  'dreiundfünfzig  ',
+  'vierundfünfzig  ',
+  'fünfundfünfzig  ',
+  'sechsundfünfzig ',
+  'siebenundfünfzig',
+  'achtundfünfzig  ',
+  'neunundfünfzig  '
 ];
 
 const drawAll = () => {
@@ -108,26 +106,26 @@ const onSecond = () => {
   //TODO: set currentDate, oldDate => all functions use these, reset after onMinute?
   g.setFont(font, fontSize);
 
-  g.setColor(colorBG);
-  g.drawString(words[currentDate.getSeconds()], startX, startYSec);
+//  g.setColor(colorBG);
+//  g.drawString(words[currentDate.getSeconds()], startX, startYSec);
   if (currentDate.getSeconds() === 59) {
     onMinute();
   }
   currentDate = new Date();
   g.setColor(colorSec);
-  g.drawString(words[currentDate.getSeconds()], startX, startYSec);
+  g.drawString(words[currentDate.getSeconds()], startX, startYSec, true);
 };
 
 
 const onMinute = () => {
-  g.setColor(colorBG);
-  g.drawString(words[currentDate.getMinutes()], startX, startYMin);
+//  g.setColor(colorBG);
+//  g.drawString(words[currentDate.getMinutes()], startX, startYMin);
   if (currentDate.getMinutes() === 59) {
     onHour();
   }
   currentDate = new Date();
   g.setColor(colorMin);
-  g.drawString(words[currentDate.getMinutes()], startX, startYMin);
+  g.drawString(words[currentDate.getMinutes()], startX, startYMin, true);
 
   if (settings.alarm1 && (currentDate.getHours() === settings.ahr && currentDate.getMinutes() === settings.amin)) {
     Bangle.buzz();
@@ -135,32 +133,32 @@ const onMinute = () => {
 };
 
 const onHour = () => {
-  g.setColor(colorBG);
-  g.drawString(words[currentDate.getHours()], startX, startYHour);
+//  g.setColor(colorBG);
+//  g.drawString(words[currentDate.getHours()], startX, startYHour);
   if (settings.date && (currentDate.getHours() === 23)) {
     onDay();
   }
   currentDate = new Date();
   g.setColor(colorHour);
-  g.drawString(words[currentDate.getHours()], startX, startYHour);
+  g.drawString(words[currentDate.getHours()], startX, startYHour, true);
 };
 
 const onDay = () => {
   let dowString = locale.dow(currentDate);
   let dateString = words[currentDate.getDate()];
   let monthString = words[currentDate.getMonth()+1];
-  g.setColor(colorBG);
-  g.drawString(dowString, startX, startYDOW);
-  g.drawString(dateString, startX, startYDate);
-  g.drawString(monthString, startX, startYMonth);
+//  g.setColor(colorBG);
+//  g.drawString(dowString, startX, startYDOW);
+//  g.drawString(dateString, startX, startYDate);
+//  g.drawString(monthString, startX, startYMonth);
   currentDate = new Date();
   dowString = locale.dow(currentDate);
   dateString = words[currentDate.getDate()];
   monthString = words[currentDate.getMonth()+1];
   g.setColor(colorDate);
-  g.drawString(dowString, startX, startYDOW);
-  g.drawString(dateString, startX, startYDate);
-  g.drawString(monthString, startX, startYMonth);
+  g.drawString(dowString, startX, startYDOW, true);
+  g.drawString(dateString, startX, startYDate, true);
+  g.drawString(monthString, startX, startYMonth, true);
 
 };
 
@@ -190,4 +188,14 @@ Bangle.drawWidgets();
 
 // Show launcher when middle button pressed
 setWatch(Bangle.showLauncher, BTN2, { repeat: false, edge: "falling" });
+
+// Stop updates when LCD is off, restart when on
+Bangle.on('lcdPower',on=>{
+  if (timer) clearInterval(timer);
+  timer = undefined;
+  if (on) {
+    setInterval(onSecond, 1000);
+    onSecond(); // draw immediately
+  }
+});
 
